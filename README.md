43751f70897447c8c9be8308b63381b86e6a1280
Jostin Fabian <Jostinfabian14@gmail.com>
2023-09-21 18:37:03 +0200

    README.MD ACABADO

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# PracticaPHP
- 
+# Persona en PHP
+## Contenido
+
+- [Persona en PHP](#persona-en-php)
+  - [Contenido](#contenido)
+  - [Actividad 1](#actividad-1)
+
+## Actividad 1
+1. Crear un arxiu que contingui HTML i PHP per donar la informació d’una persona la qual haurà de tindre els següents atributs:
+[x] nom persona : Kurt
+[x]cognoms persona: Cagle
+[x]edat de tipus numèric : 40
+[x]data de naixement (cal buscar la funció  corresponent per a introduir dates) : 1983/05/12
+[x]telèfon: 654332211, 933332211
+[x]adreça postal: Carrer de turin, 15
+[x]adreça electrònica: kurt.cagle@example.com
+[x]treballa : Si
+[x]alçada: 1,67
+
--- index.php
+++ index.php
@@ -1,11 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PracticaPHP</title>
 </head>
+
 <body>
-    
+    <?php
+    $name = "Kurt";
+    $last_name="Cagle";
+    $age = 40;
+    $date = new Datetime();
+    $date->setDate(1983, 5, 12);
+    $phones = [654332211, 933332211];
+    $street = "Carrer de turin, 15";
+    $email = "kurt.cagle@example.com";
+    $isJob = true;
+    $height = "1,67";
+    echo "<h1>Descrivint a $name $last_name</h1>";
+    echo "<h2>Les dades de $name son :</h2>";
+    echo "<ul>";
+
+    echo "<li>Es diu: $name</li>";
+    echo "<li>Te $age anys</li>";
+    echo "<li>Va neixer l'any: " . "<b>" . $date->format('Y-m-d') . "</b>";
+    echo "<li>El seus telefons son: " . $phones[0] . " - " . $phones[1] . "</li>";
+    echo "<li>Viu a: " . $street . "</li>";
+    echo "<li>El seu email és: $email</li>";
+    if ($isJob == true) {
+        echo "<li>Treballa: Si</li>";
+    } else {
+        echo "<li>Treballa: No</li>";
+    }
+    echo "<li>I medeix: $height</li>";
+    echo "</ul>";
+    ?>
+
+
+
 </body>
+
 </html>

9f52e53df62cea2a5779f45b3db4a56693ac9018
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-09-26 18:43:18 +0200

    He cambiado el titulo del ejercicio

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PracticaPHP</title>
+    <title>PRÀCTICA 2 - PERSONA i llista cursos en PHP</title>
 </head>
 
 <body>

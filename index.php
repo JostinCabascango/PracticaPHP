c0a93ab8e0f417ba65f9f7175f1a9015a44a1836
JostinCabascango <2023_jostin.cabascango@iticbcn.cat>
2023-09-26 19:52:56 +0200

    Añadido la url de la imagen de cada curso

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
     define("IVA", 0.21);
     // Definicion de los cursos en un array bidimensional
     $cursos = array(
-        array(1, "PHP", "Introducció a PHP", "15.5"),
-        array(2, "Laravel", "Laravel per experts", "30"),
-        array(3, "Django", "Dominant Django", "10")
+        array(1, "PHP", "Introducció a PHP", "15.5","https://cdn.icon-icons.com/icons2/887/PNG/512/PHP_icon-icons.com_68990.png"),
+        array(2, "Laravel", "Laravel per experts", "30","https://cdn.icon-icons.com/icons2/2248/PNG/512/laravel_icon_135451.png"),
+        array(3, "Django", "Dominant Django", "10","https://cdn.icon-icons.com/icons2/2622/PNG/512/brand_django_icon_158932.png")
     );
     // llista de cursos
     echo "<h1>Llista de cursos</h1>";
@@ -24,7 +24,7 @@
     // Recorremos el array de cursos
     foreach ($cursos as $curso) {
         echo "<div style='border: 2px solid black;padding: 0.5rem'>";
-        echo "<img style='height: 100px;width: 100px;' src='https://cdn-icons-png.flaticon.com/512/4762/4762311.png'>";
+        echo "<img style='height: 100px;width: 100px;' src='$curso[4]'>";
         echo "<h3>" . $curso[1] . "</h3>";
         echo "<p>" . $curso[2] . "</p>";
         echo "<p>El preu del llibre amb IVA : " . ($curso[3] + ($curso[3] * IVA)) . "</p>";
